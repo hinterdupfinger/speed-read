da8e4b46bf7751ebd44918d82b554a279b89092f
jonathan.lechner <16844326+hinterdupfinger@users.noreply.github.com>
2020-05-29 21:09:19 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,28 @@
+![Built With Stencil](https://img.shields.io/badge/-Built%20With%20Stencil-16161d.svg?logo=data%3Aimage%2Fsvg%2Bxml%3Bbase64%2CPD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJhdG9yIDE5LjIuMSwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246IDYuMDAgQnVpbGQgMCkgIC0tPgo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IgoJIHZpZXdCb3g9IjAgMCA1MTIgNTEyIiBzdHlsZT0iZW5hYmxlLWJhY2tncm91bmQ6bmV3IDAgMCA1MTIgNTEyOyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI%2BCjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI%2BCgkuc3Qwe2ZpbGw6I0ZGRkZGRjt9Cjwvc3R5bGU%2BCjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik00MjQuNywzNzMuOWMwLDM3LjYtNTUuMSw2OC42LTkyLjcsNjguNkgxODAuNGMtMzcuOSwwLTkyLjctMzAuNy05Mi43LTY4LjZ2LTMuNmgzMzYuOVYzNzMuOXoiLz4KPHBhdGggY2xhc3M9InN0MCIgZD0iTTQyNC43LDI5Mi4xSDE4MC40Yy0zNy42LDAtOTIuNy0zMS05Mi43LTY4LjZ2LTMuNkgzMzJjMzcuNiwwLDkyLjcsMzEsOTIuNyw2OC42VjI5Mi4xeiIvPgo8cGF0aCBjbGFzcz0ic3QwIiBkPSJNNDI0LjcsMTQxLjdIODcuN3YtMy42YzAtMzcuNiw1NC44LTY4LjYsOTIuNy02OC42SDMzMmMzNy45LDAsOTIuNywzMC43LDkyLjcsNjguNlYxNDEuN3oiLz4KPC9zdmc%2BCg%3D%3D&colorA=16161d&style=flat-square)
+
+# SpeedRead 📖
+
+A webcomponent for speed reading, to reduce the need of eye movement.
+
+## Usage
+
+You can include it in your website like any other webcomponent (https://stenciljs.com/docs/overview).
+
+Example usage (for more detail check the component Readme):
+
+```
+<speed-read wpm="250" text="<YOU_TEXT>"></speed-read>
+```
+
+By clicking on the component you can start and pause the reading, but there are also exposed functions you can call to start and stop the reading with the buttons in you websites styles.
+
+To get the text you could either directly pass the article of your cms or use https://github.com/mozilla/readability to extract it again of any website.
+
+## Styling
+
+To also adjust the appearance there are css costom variables to adjust the fonts and colors:
+
+- `--speed-read-font-size`
+- `--speed-read-font-family`
+- `--speed-read-color-text`
+- `--speed-read-color-text-focus`
---
+++ src/components/speed-read/speed-read.tsx
@@ -0,0 +1,121 @@
+import { Component, h, Method, Prop, State, Watch } from "@stencil/core";
+
+@Component({
+  tag: "speed-read",
+  styleUrl: "speed-read.css",
+  shadow: true,
+})
+export class SpeedRead {
+  private readonly SHORT_WORD_LENGTH: number = 9;
+  private readonly MS_PER_MINUTE = 60000;
+
+  private words: string[] = [];
+  private currentTimerId: number | undefined;
+
+  @State() currentIndex: number = 0;
+
+  @Prop() wpm: number = 450;
+
+  @Method()
+  async start() {
+    this.currentTimerId = setInterval(() => {
+      this.currentIndex++;
+      if (this.currentIndex >= this.words.length) {
+        this.stop();
+      }
+    }, this.MS_PER_MINUTE / this.wpm);
+  }
+
+  @Method()
+  async pause() {
+    if (this.currentTimerId) {
+      clearInterval(this.currentTimerId);
+      this.currentTimerId = undefined;
+    }
+  }
+
+  @Method()
+  async stop() {
+    this.currentIndex = 0;
+    this.pause();
+  }
+
+  @Prop() text: string;
+
+  @Watch("text")
+  splitToWords(newValue: string, oldValue: string) {
+    if (newValue !== oldValue) {
+      this.stop();
+
+      const allWords = newValue.split(/\s+/);
+
+      this.words = allWords
+        .flatMap((word) => {
+          word = word.trim();
+
+          if (word.length > 0) {
+            if (
+              word.length > this.SHORT_WORD_LENGTH ||
+              word.includes(",") ||
+              word.includes(":") ||
+              word.includes("-") ||
+              word.includes("(") ||
+              word.includes(".") ||
+              word.includes("!") ||
+              word.includes("?") ||
+              word.includes(":") ||
+              word.includes(";") ||
+              word.includes(")")
+            ) {
+              return [word, word];
+            }
+          }
+          return [word];
+        })
+        .filter((word) => !!word || word !== "");
+    }
+  }
+
+  render() {
+    if (this.words.length === 0 && this.text !== "") {
+      this.splitToWords(this.text, "");
+    }
+
+    return (
+      <div class="reader" onClick={() => this.clickHandler()}>
+        {this.currentIndex < this.words.length
+          ? this.findFocusLetter(this.words[this.currentIndex])
+          : ""}
+      </div>
+    );
+  }
+
+  findFocusLetter(word: string) {
+    let start: string;
+    let focus: string;
+    let end: string;
+
+    if (word.length > 1) {
+      const center = word.length / 2;
+
+      start = word.slice(0, center - 1);
+      focus = word.slice(center - 1, center);
+      end = word.slice(center, word.length);
+    } else {
+      focus = word;
+    }
+    return [
+      <span class="start">{start}</span>,
+      <span class="focus">{focus}</span>,
+      <span class="end">{end}</span>,
+    ];
+  }
+
+  private clickHandler() {
+    if (this.currentTimerId) {
+      this.pause();
+    } else {
+      this.start();
+    }
+  }
+}
